13a7bbb2515f80813efac22cea50539b689cf5c5
Jefferson Diaz <djefferson545@gmail.com>
2023-07-01 23:27:16 -0700

    added config service initial props.

---
+++ src/test/java/com/chatapp/configserver/ConfigServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.chatapp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
